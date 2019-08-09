d9d60522e50101c32e9359d1dc6983046583b225
MichaelHMods <mikeguadalupe@gmail.com>
2019-08-08 19:13:11 -0500

    final copy

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,10 @@
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
+
 */
+
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +27,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = [https://api.github.com/users/<Your github name>/followers];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,8 +48,81 @@ const followersArray = [];
 </div>
 
 */
+function gitData(obj){
 
-/* List of LS Instructors Github username's: 
+  const newCard = document.createElement('div');
+  newCard.classList.add('card');
+
+  const imgSrc1 = document.createElement('img');
+  imgSrc1.setAttribute('src', obj.avatar_url)
+
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+
+  const hThree = document.createElement('h3');
+  hThree.classList.add("name");
+
+  const uName = document.createElement('p');
+  uName.classList.add('username');
+
+  const theLocation = document.createElement("p");
+
+  const profile = document.createElement('p');
+
+  const gitAddy = document.createElement('a');
+  gitAddy.setAttribute('href', obj.html_url);
+
+  const followers = document.createElement('p');
+
+
+  const following = document.createElement('p');
+
+  const theBio = document.createElement('p');
+
+  // set structure
+  newCard.appendChild(imgSrc1);
+  newCard.appendChild(cardInfo);
+  cardInfo.appendChild(hThree);
+  cardInfo.appendChild(uName);
+  cardInfo.appendChild(theLocation);
+  cardInfo.appendChild(profile);
+  profile.appendChild(gitAddy);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+
+  // set text content src
+  imgSrc1.textContent = obj.avatar_url;
+  hThree.textContent = obj.name; 
+  uName.textContent = obj.login;
+  theLocation.textContent = obj.location;
+  gitAddy.textContent = obj.html_url;
+  followers.textContent = `Follower ${obj.followers}`;
+  following.textContent = `Following ${obj.following}`;
+  theBio.textContent = obj.bio;
+
+  return newCard;
+};
+
+
+const theCards = document.querySelector('.cards');
+const followersArray = [
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell'
+]
+
+followersArray.forEach(person => {
+  axios.get(`https://api.github.com/users/${person}`)
+  .then((response) => theCards.appendChild(gitData(response.data)))
+});
+// axios.get('https://api.github.com/users/MichaelHMods')
+//   .then((response) => { 
+//     theCards.appendChild(gitData(response.data));
+//   });
+/* List of LS Instructors Github username's:/* 
+ 
   tetondan
   dustinmyers
   justsml
